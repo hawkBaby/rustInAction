7c68146314b307026e7f240610cc75d55953d653
Jad Pamment <jad.apamment@gmail.com>
2020-11-08 17:25:21 +1300

    Updating grep and adding in ch2-read-file.rs

--- Chapter2/src/ch2-with-regex.rs
+++ Chapter2/src/ch2-with-regex.rs
@@ -1,19 +1,7 @@
 use regex::Regex;
-use clap::{App,Arg};
 
 fn main() {
-
-    let args = App::new("grep-lite")
-        .version("0.1")
-        .about("searches for patterns")
-        .arg(Arg::with_name("pattern")
-            .help("The pattern to search for")
-            .takes_value(true)
-            .required(true))
-        .get_matches();
-
-    let pattern = args.value_of("pattern").unwrap();
-    let re = Regex::new(pattern).unwrap();
+    let re = Regex::new("picture").unwrap();
 
     let quote = "\
     Every face, every shop, bedroom window, public-house, 
